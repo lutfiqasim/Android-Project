bd1326db85e736b09e4c0a06e5ac7fad313648d9
lutfiqasim <lutfiqasim2002@gmail.com>
2023-06-30 22:44:49 +0300

    1

--- app/src/main/java/edu/birzeit/chatproject/MessageAdapter.java
+++ app/src/main/java/edu/birzeit/chatproject/MessageAdapter.java
@@ -9,11 +9,15 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import java.util.List;
 
-public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageViewHolder> {
+public class MessageAdapter extends RecyclerView.Adapter {
 
     private List<MessageModel> messageList;
+    final int ItemSend = 1;
+    final int Itemrecived = 2;
 
     public MessageAdapter(List<MessageModel> messageList) {
         this.messageList = messageList;
@@ -22,19 +26,40 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
     @NonNull
     @Override
     public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_message, parent, false);
-        return new MessageViewHolder(view);
+        if (viewType == ItemSend) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_message, parent, false);
+            return new MessageViewHolder(view);
+        } else {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_sender_message, parent, false);
+            return new MessageViewHolder(view);
+        }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MessageViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         MessageModel message = messageList.get(position);
-        holder.bind(message);
+        if (holder.getClass() == MessageViewHolder.class) {
+            MessageViewHolder viewHolder = (MessageViewHolder) holder;
+            viewHolder.bind(message);
+        } else {
+            SenderViewHolder viewHolder = (SenderViewHolder) holder;
+            viewHolder.bind(message);
+        }
     }
 
     @Override
     public int getItemCount() {
         return messageList.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {//Checks for view type of message sent or recived
+        MessageModel message = messageList.get(position);
+        if (FirebaseAuth.getInstance().getCurrentUser().getUid().equals((message.getSenderId()))) {
+            return ItemSend;
+        } else {
+            return Itemrecived;
+        }
     }
 
     public class MessageViewHolder extends RecyclerView.ViewHolder {
@@ -44,6 +69,28 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
         TextView senderName;
 
         public MessageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            senderImage = itemView.findViewById(R.id.senderImage);
+            messageTextView = itemView.findViewById(R.id.messageTextView);
+            time = itemView.findViewById(R.id.editTextTime);
+            senderName = itemView.findViewById(R.id.senderName);
+        }
+
+        public void bind(MessageModel message) {
+            senderImage.setImageResource(R.drawable.photocamera);
+            messageTextView.setText(message.getMessageText());
+            time.setText(message.getTimestamp() + "");
+            senderName.setText(message.getSenderId());//Change it to name using sql
+        }
+    }
+
+    public class SenderViewHolder extends RecyclerView.ViewHolder {
+        ImageView senderImage;
+        TextView messageTextView;
+        TextView time;
+        TextView senderName;
+
+        public SenderViewHolder(@NonNull View itemView) {
             super(itemView);
             senderImage = itemView.findViewById(R.id.senderImage);
             messageTextView = itemView.findViewById(R.id.messageTextView);
