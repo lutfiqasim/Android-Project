89116dfc134421767d779d73a6546ec066a461f2
Basel Z. Abu Hamed <basel76ziyad24@gmail.com>
2023-07-10 21:53:52 +0300

    desgin the chat feature

--- app/src/main/java/edu/birzeit/chatproject/HomeFragment.java
+++ app/src/main/java/edu/birzeit/chatproject/HomeFragment.java
@@ -1,19 +1,30 @@
 package edu.birzeit.chatproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
 
+    private ImageView chats;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        chats = rootView.findViewById(R.id.chats);
+        chats.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), ChatWindow.class);
+            startActivity(intent);
+        });
+        return rootView;
     }
 }
